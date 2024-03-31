9fdbac248f91d9ce9190d5b03e57dee4bd072439
Kuei-Feng Tung <57666754+omgitskuei@users.noreply.github.com>
2024-03-31 16:08:40 +0800

    Create utils/validator

---
+++ utils/Validator.tsx
@@ -0,0 +1,89 @@
+type result = {
+    isValid: boolean,
+    message: string
+}
+
+/**
+ * Given a string, check if it is a valid email
+ * Return an object with two keys: isValid (bool), message (string)
+ * If valid, then isValid is true and message is not empty ("").
+ * @param email string
+ * @returns 
+ */
+export function validateEmail(email : string) {
+    // Populate default values for result object
+    let result : result = {
+        isValid: true,
+        message: ""
+    }
+
+    // rulesets for validity
+    if (email.length == 0) {
+        result.isValid = false;
+        result.message = "* 這為必填欄位";
+    }
+
+    return result;
+}
+
+/**
+ * Given a string, check if it is a valid username
+ * Return an object with two keys: isValid (bool), message (string)
+ * If valid, then isValid is true and message is not empty ("").
+ * @param username string
+ * @returns 
+ */
+export function validateUsername(username : string) {
+    // Populate default values for result object
+    let result : result = {
+        isValid: true,
+        message: ""
+    }
+
+    // rulesets for validity
+    if (username.length == 0) {
+        result.isValid = false;
+        result.message = "* 這為必填欄位";
+    }
+
+    return result;
+}
+
+/**
+ * Given a string, check if it is a valid password
+ * Return an object with two keys: isValid (bool), message (string)
+ * If valid, then isValid is true and message is not empty ("").
+ * @param pw string
+ * @returns 
+ */
+export function validatePassword(pw: string) {
+    // Populate default values for result object
+    let result : result = {
+        isValid: true,
+        message: ""
+    }
+
+    // rulesets for validity
+    if (pw.length == 0) {
+        result.isValid = false;
+        result.message = "* 這為必填欄位";
+    }
+    if (pw.length < 8) {
+        result.isValid = false;
+        result.message = "* 密碼不得短於8個字符";
+    }
+    if (!(/[A-Z]/.test(pw))) {
+        result.isValid = false;
+        result.message = "* 密碼需至少一個英文大寫字符";
+    }
+    if (!(/[a-z]/.test(pw))) {
+        result.isValid = false;
+        result.message = "* 密碼需至少一個英文小寫字符";
+    }
+    if (!(/[0-9]/.test(pw))) {
+        result.isValid = false;
+        result.message = "* 密碼需至少一個數字";
+    }
+
+    return result;
+}
